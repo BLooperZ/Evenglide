3d040a823de6b9600d88b3b6dce36bbec2f2cbc0
BLooperZ <blooperz@users.noreply.github.com>
2025-08-01 12:44:31 +0300

    Fix segments rotation

--- rope2d.gd
+++ rope2d.gd
@@ -20,20 +20,26 @@ func _ready() -> void:
 	add_child(seg)
 	var last_seg = seg
 	var joint = create_joint()
+	joint.global_position = seg.global_position
 	add_child(joint)
 	joint.node_a = joint.get_path_to(player1)
 	joint.node_b = joint.get_path_to(seg)
+	
+	var vec = (player2.global_position - player1.global_position) / num_segments
 
 	for i in range(num_segments):
 		seg = segment_scene.instantiate()
 		add_child(seg)
+		seg.global_position = last_seg.global_position + vec
 		joint = create_joint()
+		joint.global_position = (last_seg.global_position + seg.global_position) / 2
 		add_child(joint)
 		joint.node_a = joint.get_path_to(last_seg)
 		joint.node_b = joint.get_path_to(seg)
 		last_seg = seg
 
 	joint = create_joint()
+	joint.global_position = last_seg.global_position
 	add_child(joint)
 	joint.node_a = joint.get_path_to(player2)
 	joint.node_b = joint.get_path_to(last_seg)
