2d29d7eb9659bcf871b45ef2dfa6a6e57208e3ae
BLooperZ <blooperz@users.noreply.github.com>
2025-08-01 12:09:44 +0300

    Improve character movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 1000.0
+const TURN_RATE = 0.035
+const ACCELERATION = 0.01
 
 @export var key_up: String = "p1_up"
 @export var key_down: String = "p1_down"
@@ -10,15 +12,38 @@ const SPEED = 1000.0
 
 @export var joint: Node2D = null
 
+var direction := Vector2.ZERO;
+var wish_dir := Vector2.ZERO;
+var commitment := 0.
+
 func _physics_process(delta: float) -> void:
+    # Get the input direction and handle the movement/deceleration.
+    # As good practice, you should replace UI actions with custom gameplay actions.
+    wish_dir = Input.get_vector(key_left, key_right, key_up, key_down)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector(key_left, key_right, key_up, key_down)
-	velocity = lerp(velocity, direction * SPEED, 0.7)
+    if direction == Vector2.ZERO:
+        direction = wish_dir
 
-	velocity -= joint.force * delta * (velocity.length() / SPEED)
+    direction = direction.rotated(lerp_angle(0, 0 + direction.angle_to(wish_dir), TURN_RATE)).normalized()
+    
+    commitment = lerp(commitment, wish_dir.length(), ACCELERATION)
 
-	#velocity *= 0.7
+    velocity = lerp(velocity, direction * SPEED * commitment, 0.7)
 
-	move_and_slide()
+    velocity -= joint.force * delta * (velocity.length() / SPEED)
+    
+
+    move_and_slide()
+    queue_redraw()
+
+func _draw():
+    if wish_dir != Vector2.ZERO:
+        var line_length = 50.0
+        draw_line(Vector2.ZERO, (wish_dir * line_length), Color.RED, 4.0)
+    if direction != Vector2.ZERO:
+        var line_length = 100.0
+        draw_line(Vector2.ZERO, (direction * line_length), Color.MAGENTA, 4.0)
+        draw_line(Vector2.ZERO, (direction * commitment * line_length), Color.GREEN, 4.0)
+    if velocity != Vector2.ZERO:
+        var line_length = 150.0
+        draw_line(Vector2.ZERO, (velocity.normalized() * line_length), Color.PURPLE, 2.0)
