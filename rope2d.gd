b55d179544e0dacdf6396d79502cb89f660cdf46
BLooperZ <blooperz@users.noreply.github.com>
2025-08-03 00:04:25 +0300

    Position resilient rope creation

--- rope2d.gd
+++ rope2d.gd
@@ -8,11 +8,10 @@ class_name Rope2D
 
 @export var num_segments = 8
 
+
 func create_joint():
 	var joint = PullJoint2D.new()
 	joint.softness = 16.0
-	joint.k = 400.0
-	joint.rest_length = 20.0
 	joint.angular_limit_enabled = true
 	joint.angular_limit_lower = -3
 	joint.angular_limit_upper = 3
@@ -20,18 +19,24 @@ func create_joint():
 
 var line_data_sources: Array[Node2D] = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func create_segments() -> void:
+	line_data_sources.clear()
+
+	for child in get_children():
+		remove_child(child)
+		child.queue_free()
+	var vec = (player2.global_position - player1.global_position) / num_segments
+
 	var seg = segment_scene.instantiate()
+	seg.global_position = player1.global_position
+	seg.global_rotation = PI / 2 + (player2.global_position - player1.global_position).angle()
 	add_child(seg)
 	var last_seg = seg
 	var joint = create_joint()
-	joint.global_position = seg.global_position
-	add_child(joint)
+	#joint.global_position = seg.global_position
+	seg.add_child(joint)
 	joint.node_a = joint.get_path_to(player1)
 	joint.node_b = joint.get_path_to(seg)
-
-	var vec = (player2.global_position - player1.global_position) / num_segments
 
 	line_data_sources.append(player1);
 	line_data_sources.append(seg);
@@ -40,26 +45,34 @@ func _ready() -> void:
 		seg = segment_scene.instantiate()
 		add_child(seg)
 		seg.global_position = last_seg.global_position + vec
+		seg.global_rotation = PI / 2 + (last_seg.global_position - seg.global_position).angle()
 		seg.prev = last_seg
 		last_seg.next = seg
 		joint = create_joint()
-		joint.global_position = (last_seg.global_position + seg.global_position) / 2
-		add_child(joint)
+		#joint.global_position = (last_seg.global_position + seg.global_position) / 2
+		seg.add_child(joint)
 		joint.node_a = joint.get_path_to(last_seg)
 		joint.node_b = joint.get_path_to(seg)
 		last_seg = seg
-		line_data_sources.append(seg);
-
+		line_data_sources.append(seg)
 
 	joint = create_joint()
-	joint.global_position = last_seg.global_position
-	add_child(joint)
+	#joint.global_position = last_seg.global_position
+	seg.add_child(joint)
 	joint.node_a = joint.get_path_to(player2)
 	joint.node_b = joint.get_path_to(last_seg)
 
 	line_data_sources.append(player2);
 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	create_segments()
+
 func _physics_process(_delta: float) -> void:
 	clear_points()
 	for link in line_data_sources:
 		add_point(link.global_position)
+
+	if Input.is_action_just_released("ui_accept"):
+		create_segments()
